b2c64d437fe16acfc6de1b60f5ac608564a3c25c
Dereck <dereck@keemail.me>
2021-10-04 16:51:23 -0600

    update Regex file

--- Regular_Expressions/regExTest.py
+++ Regular_Expressions/regExTest.py
@@ -2,5 +2,5 @@ import re
 
 phoneNumRegex = re.compile(r'(\d\d\d) (\d\d\d\d-\d\d\d\d)')
 
-mo = phoneNumRegex.search('Is the number 502 5980-8265cle?')
+mo = phoneNumRegex.search('Is the number 502 5980-8265 ?')
 print(mo.group(0))
