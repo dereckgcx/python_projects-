53251fac9ddab52710a64a4070d1c89aabb77ed4
Dereck <dereck@keemail.me>
2021-10-04 16:57:00 -0600

    add groups() module

--- Regular_Expressions/regExTest.py
+++ Regular_Expressions/regExTest.py
@@ -1,6 +1,11 @@
 import re
 
-phoneNumRegex = re.compile(r'(\d\d\d) (\d\d\d\d-\d\d\d\d)')
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d\d-\d\d\d\d)')
 
-mo = phoneNumRegex.search('Is the number 502 5980-8265 ?')
+mo = phoneNumRegex.search('Is the number (502) 5980-8265 ?')
 print(mo.group(0))
+
+areaCode, mainNum = mo.groups()
+
+print (f'Area Code: {areaCode}')
+print (f'Number: {mainNum}')
