a20b02f19fd55d60c744c972c5d7513d4258a9ee
Dereck <dereck@keemail.me>
2021-10-04 15:11:22 -0600

    add looking for number in text functionality

--- regExp.py
+++ regExp.py
@@ -11,6 +11,10 @@ def isPhoneNumber(text):
             return False
     return True
 
-print (isPhoneNumber('4758-0593'))
-#512-512-1252
-#5970-9269
+message = 'Call me tomorrow at 5970-9269 tomorrow, the office is 6661-6139.'
+
+for i in range (len(message)):
+    chunk = message [i:i+9]
+    if isPhoneNumber(chunk):
+        print(f'Phone number: {chunk}')
+print ('Done')
