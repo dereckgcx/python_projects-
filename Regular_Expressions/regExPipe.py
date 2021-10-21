ad24c91cfc9cd68bb4d56c0433df2445298d5abc
Dereck <dereck@keemail.me>
2021-10-20 19:15:50 -0600

    add RegEx optional expressions examples

--- Regular_Expressions/regExPipe.py
+++ Regular_Expressions/regExPipe.py
@@ -14,3 +14,18 @@ mo = batRegex.search('Batmobile lost a wheel')
 mo.group()
 
 print(mo.group(1))
+
+#optional pattern matching using "?" character
+
+batRegex = re.compile(r'Bat(wo)?man') #this section specifies the optional part
+mo3 = batRegex.search('The Adventures of Batwoman')
+print (mo3.group())
+
+#
+
+phoneRegex = re.compile(r'(\(\d\d\d\))?\d\d\d\d-\d\d\d\d')
+mox = phoneRegex.search('My current phone number is (502)5970-9268')
+print (mox.group())
+
+mox1 = phoneRegex.search('My current phone number is 4136-6940')
+print (mox1.group())
