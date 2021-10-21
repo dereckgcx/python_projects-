a3858011392afc93da4e483baffe60d1c816b6ac
Dereck <dereck@keemail.me>
2021-10-20 19:54:12 -0600

    add RegEx one or more

--- Regular_Expressions/oneOrMore.py
+++ Regular_Expressions/oneOrMore.py
@@ -2,3 +2,9 @@
 
 import re
 
+batRegex = re.compile(r'Bat(wo)+man') #this means such group in paranthesis must appear at least once, for instance Batman will not have any matches. 
+mo1 = batRegex.search('The Adventures of Batwoman')
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+mo3 = batRegex.search('The Adventures of Batman')
+
+print (f'{mo1.group()}\n {mo2.group()} \n {mo3 == None}')
