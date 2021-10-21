e62c84f295c0821cad28aaf630c5a8ea2450701b
Dereck <dereck@keemail.me>
2021-10-20 19:48:30 -0600

    add RegEx zero or more

---
+++ Regular_Expressions/oneOrMore.py
@@ -0,0 +1,4 @@
+#the "+will match one or more 
+
+import re
+
--- Regular_Expressions/zeroOrMore.py
+++ Regular_Expressions/zeroOrMore.py
@@ -1,1 +1,14 @@
-#
+# the star/asterisk "*" character means match zero or more.
+# the group that precedes such can occur any number of times to coincide for a match
+ 
+import re
+
+batRegex = re.compile(r'Bat(wo)*man')
+mou1 = batRegex.search('The adventures of Batwowowowowowowowoman')
+mou2 = batRegex.search('The adventures of Batwowoman')
+mou3 = batRegex.search('The adventures of Batwoman')
+
+print   (mou1.group(),'\n',
+        mou2.group(),'\n',
+        mou3.group())
+
